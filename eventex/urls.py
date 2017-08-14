3f2a56d3c53e10e54d8f3992f157cf906bf0f23a
Luciano Costa <lcosta_96@hotmail.com.br>
2017-08-14 10:57:05 -0300

    New app created

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,5 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'eventex.core.views.home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
