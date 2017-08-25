d494f1b28e84b05e5a514bb6b212d7463fe89ab0
Luciano Costa <lcosta_96@hotmail.com.br>
2017-08-25 16:21:52 -0300

    CPF validation

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -2,20 +2,18 @@ from django.test import TestCase
 from django.shortcuts import resolve_url as r
 from eventex.subscriptions.forms import SubscriptionForm
 
+
 class SubscriptionsNewGet(TestCase):
     def setUp(self):
         self.response = self.client.get(r('subscriptions:new'))
-
 
     def test_get(self):
         """ Get '/inscricao/ must return status code 200 """
         self.assertEqual(200, self.response.status_code)
 
-
     def test_template(self):
         """ Must use 'subscriptions/subscription_form.html' """
         self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
-
 
     def test_html(self):
         """ HTML must contain input tags """
@@ -25,19 +23,40 @@ class SubscriptionsNewGet(TestCase):
         self.assertContains(self.response, 'type="email"')
         self.assertContains(self.response, 'type="submit"')
 
-
     def test_csfr(self):
         """ HTML must contain csfr"""
         self.assertContains(self.response, 'csrfmiddlewaretoken')
-
 
     def test_has_form(self):
         """ Context must have subscription form """
         form = self.response.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-
     def test_form_has_fields(self):
         """ Form must have 4 fields """
         form = self.response.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
+
+    def test_cfp_is_digit(self):
+        """ CFP must only accept digits """
+        form = self.make_validated_form(cpf='ABCD567890')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_eleven_digits(self):
+        """ CPF must have 11 digits """
+        form = self.make_validated_form(cpf='1234')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='John Doe', cpf='12345678901',
+                     email='johndoe@email.com', phone='98-984743028')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
